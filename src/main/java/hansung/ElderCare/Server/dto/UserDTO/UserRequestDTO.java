deb6dc0c15a87abf08b55127ec817d3b590c738e
HSSJW <okqkfkfk@naver.com>
2025-02-25 14:32:10 +0900

    [#17] Feat : 사용자 정보 조회 api 구현

--- src/main/java/hansung/ElderCare/Server/dto/UserDTO/UserRequestDTO.java
+++ src/main/java/hansung/ElderCare/Server/dto/UserDTO/UserRequestDTO.java
@@ -2,6 +2,7 @@ package hansung.ElderCare.Server.dto.UserDTO;
 
 import hansung.ElderCare.Server.domain.enums.Relationship;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,15 +17,19 @@ public class UserRequestDTO {
     @NoArgsConstructor
     public static class UserInfoEditDTO {
 
+        @NotBlank(message = "사용자이름은 필수 입력사항입니다.")
         @Schema(description = "보호자(사용자) 이름", example = "송진우")
         private String name;
 
+        @NotBlank
         @Schema(description = "인증 이미지 파일 (multipart/form-data)", type = "string", format = "binary")
         private MultipartFile profileImage;
 
+        @NotBlank(message = "전화번호는 필수 입력사항입니다.")
         @Schema(description = "전화번호", example = "01011112222")
         private String phoneNumber;
 
+        @NotBlank(message = "보호 대상자와의 관계를 입력하세요")
         @Schema(description = "보호대상자와의 관계", example = "PARENT")
         private String relationship;
     }
