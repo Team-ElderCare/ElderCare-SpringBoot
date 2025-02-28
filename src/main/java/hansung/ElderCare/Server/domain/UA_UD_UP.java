8fe433eb7025f50f96c03f2955942fd7105c9ba3
HSSJW <okqkfkfk@naver.com>
2025-02-28 10:04:54 +0900

    [#43] Refactor : 도메인 수정

--- src/main/java/hansung/ElderCare/Server/domain/UA_UD_UP.java
+++ src/main/java/hansung/ElderCare/Server/domain/UA_UD_UP.java
@@ -24,6 +24,6 @@ public class UA_UD_UP {
     private Protected Protected;        // Protected 외래키
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "device_id")
-    private Device device;
+    @JoinColumn(name = "device_id") 
+    private Device device; //Device 외래키
 }
